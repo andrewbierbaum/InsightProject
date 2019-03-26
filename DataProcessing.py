dd26d7fdb6d4df453580478504ab2d973a0916ed
Andrew Bierbaum <andrew.bierbaum@gmail.com>
2019-03-26 12:28:02 -0700

    Fixed remaking a table if not needed, cleaned up

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,6 +1,7 @@
 import csv
 import sqlite3
 import pandas
+import numpy
 import sys
 import json
 import os
@@ -9,55 +10,39 @@ import datetime
 import matplotlib
 import matplotlib.pyplot as plt
 
-#https://stackoverflow.com/questions/2887878/importing-a-csv-file-into-a-sqlite3-database-table-using-python
-#REDDIT data
-#https://github.com/dewarim/reddit-data-tools
-#initialize sqlite and dump reddit data into it
+#initialize the sql DB
 conn = sqlite3.connect('twitter.db')
 cur = conn.cursor()
 DATASET_COLUMNS = ["target", "ids", "date", "flag", "user", "text"]
-cur.execute("CREATE TABLE twitter (target, ids, date, flag, user,text);")
 
-#https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
-#https://www.kaggle.com/kazanova/sentiment140/version/2
-df = pandas.read_csv('TwitterSmall.csv', encoding='ISO-8859-1', names=DATASET_COLUMNS)
+#don't rebuild the table if it already exists
+cur.execute("SELECT count(*) FROM sqlite_master WHERE type ='table'")
+if cur.fetchall()[0] == 0:
+    cur.execute("CREATE TABLE twitter (target, ids, date, flag, user,text);")
+    #read the csv  
+    df = pandas.read_csv('TwitterSmall.csv', encoding='ISO-8859-1', names=DATASET_COLUMNS)
+    #load into sql
+    df.to_sql('twitter', conn, if_exists='replace', index=False)
 
-#testing to see if loading is working
-#print(len(df))
-#print(df.head(5))
-df.to_sql('twitter', conn, if_exists='replace', index=False)
-
+#do SQL query and fetch results
 cur.execute("SELECT date FROM twitter WHERE text LIKE '%c++%'")
 times = cur.fetchall()
+
+#clean data (data,) - has a useless touple & produce a count
 cleantime = [i[0] for i in times]
+#print(times)
+count = numpy.arange(len(cleantime))
+#print(count)
 
-
-#print(cleantime)
+#clean up dates and time getting ready for matplotlib
 DateData = matplotlib.dates.datestr2num(cleantime)
 DateData.sort()
-#print(DateData)
-
-#count = [i for i in range(0,cleantime)]
-count = []
-for i in range(len(DateData)):
-    count.append(i)
-
-
-#there is a better way to do this
-#maybe come back to for counting data
-# DateAndCount = []
-# #reverse enumberate that is non-iterable
-# count = 0
-# for date in DataData
-#     DateAndCount = date
-# CountAndDate = enumerate(DateData)
-# print(CountAndDate)
 
 #plot the data
 matplotlib.pyplot.plot_date(DateData,count,xdate=True, drawstyle = 'steps', linestyle = 'solid' )
 matplotlib.pyplot.ylabel('Mentions')
 matplotlib.pyplot.title('c++ twitter mentions')
-plt.show()
+plt.savefig('TwitterC.png')
 
 #clean up sql
 conn.commit()
