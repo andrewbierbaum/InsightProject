84c6cce75f76ff077f228ef7951ee88396cdad38
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2019-04-04 00:45:31 +0000

    Got Microsoft on HackerNews

--- .ipynb_checkpoints/Dash-checkpoint.py
+++ .ipynb_checkpoints/Dash-checkpoint.py
@@ -6,7 +6,7 @@ import pandas
 from datetime import datetime
 import numpy
 
-df = pandas.read_csv("dash.csv")
+df = pandas.read_csv("HackerNews_Microsoft.csv")
 Date_Data = df['date'].tolist()
 Body_Data = df['body'].tolist()
 count = df['Unnamed: 0'].tolist()
@@ -19,17 +19,17 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Reddit Mentions'),
+    html.H1(children='HackerNews Mentions'),
 
     html.Div(children='''
-        Tracking the number of mentions microsoft got on Reddit
+        Tracking the number of mentions microsoft got on HackerNews
     '''),
 
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': Date_Data, 'y': count, 'text': Body_Data,'type': 'scatter', 'name': 'Reddit Microsoft Mentions'}
+                {'x': Date_Data, 'y': count, 'text': Body_Data,'type': 'scatter', 'name': 'HackerNews Microsoft Mentions'}
             ],
             'layout': {
                 'title': 'Dash Data Visualization'
