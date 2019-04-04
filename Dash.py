33b8992e7d6859340dcbb5508fe8a18252f7c74f
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2019-04-04 00:01:38 +0000

    Body Data is output to csv (ascii issues) and dash correctly uses

--- Dash.py
+++ Dash.py
@@ -4,28 +4,32 @@ import dash_html_components as html
 import csv
 import pandas
 from datetime import datetime
+import numpy
 
 df = pandas.read_csv("dash.csv")
-timestamps = df['timestamp'].tolist()
+Date_Data = df['date'].tolist()
+Body_Data = df['body'].tolist()
 count = df['Unnamed: 0'].tolist()
-datetimes = [datetime.fromtimestamp(i) for i in timestamps]
+
+#i think I already did this and Its not needed
+#datetimes = [datetime.fromtimestamp(i) for i in timestamps]
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Reddit Mentions'),
 
     html.Div(children='''
-        Dash: A web application framework for Python.
+        Tracking the number of mentions microsoft got on Reddit
     '''),
 
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': datetimes, 'y': count, 'type': 'scatter', 'name': 'SF'}
+                {'x': Date_Data, 'y': count, 'text': Body_Data,'type': 'scatter', 'name': 'Reddit Microsoft Mentions'}
             ],
             'layout': {
                 'title': 'Dash Data Visualization'
