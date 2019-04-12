d771b70d87610bd0a595975ccbb34c28d2e1d57b
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2019-04-12 20:07:34 +0000

    user comments and links working, polish remaining

--- Dash.py
+++ Dash.py
@@ -1,6 +1,8 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_daq as daq
 import csv
 import pandas
 from datetime import datetime
@@ -43,6 +45,7 @@ df = None
 #take in reddit data
 df = pandas.read_sql("SELECT * FROM Reddit_xamarin", conn)
 reddit_xamarin_Date_Data = [datetime.fromtimestamp(float(time)) for time in df['created_utc']]
+reddit_xamarin_link_Id_Data = df['link_id'].tolist()
 reddit_xamarin_Id_Data = df['id'].tolist()
 reddit_xamarin_Body_Data = df['body'].tolist()
 reddit_xamarin_count = numpy.arange(len(reddit_xamarin_Id_Data))
@@ -50,6 +53,7 @@ df = None
 
 df = pandas.read_sql("SELECT * FROM Reddit_react_native",conn)
 reddit_react_native_Date_Data = [datetime.fromtimestamp(float(time)) for time in df['created_utc']]
+reddit_react_native_link_Id_Data = df['link_id'].tolist()
 reddit_react_native_Id_Data = df['id'].tolist()
 reddit_react_native_Body_Data = df['body'].tolist()
 reddit_react_native_count = numpy.arange(len(reddit_react_native_Id_Data))
@@ -57,6 +61,7 @@ df = None
 
 df = pandas.read_sql("SELECT * FROM Reddit_flutter", conn)
 reddit_flutter_Date_Data = [datetime.fromtimestamp(float(time)) for time in df['created_utc']]
+reddit_flutter_link_Id_Data = df['link_id'].tolist()
 reddit_flutter_Id_Data = df['id'].tolist()
 reddit_flutter_Body_Data = df['body'].tolist()
 reddit_flutter_count = numpy.arange(len(reddit_flutter_Id_Data))
@@ -65,20 +70,84 @@ df = None
 #close the SQL 
 conn.close()
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+
+
+# theme = {
+#     'dark': False,
+#     'detail': '#007439',
+#     'primary': '#00EA64', 
+#     'secondary': '#6E6E6E'
+# }
+
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/anon/pen/mardKv.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+# theme =  {
+#     'dark': True,
+#     'detail': '#007439',
+#     'primary': '#00EA64',
+#     'secondary': '#6E6E6E',
+# }
 
-app.layout = html.Div(children=[
+
+# rootLayout = html.Div([
+#     daq.BooleanSwitch(
+#         on=True,
+#         id='darktheme-daq-booleanswitch',
+#         className='dark-theme-control'
+#     ), html.Br(),
+#     daq.ToggleSwitch(
+#         id='darktheme-daq-toggleswitch',
+#         className='dark-theme-control'
+#     ), html.Br(),
+# ])
+
+app.layout = html.Div(id='dark-theme-feature',children=[
     html.H1(children='Technology Mentions on HackerNews and Reddit',style={'text-align': 'center'}),
 #	html.Div(children ='user data',id='text-context'),
     html.Div(children='''Hover and click data to display the user comment''',style={'text-align': 'center','font-size': 24}),
+    html.Br(),
 #    #builds the year range selector
 #    dcc.RangeSlider(id='year_slider', min=2008, max=2020, value=[2008, 2020])
-# May 16, 2011
+#    May 16, 2011
     
-    html.Div([
+#      daq.ToggleSwitch(
+#         id='toggle-theme',
+#         label=['Light', 'Dark'],
+#         value=True
+#     ),
+    
+#     daq.ToggleSwitch(
+#         id='daq-light-dark-theme',
+#         label=['Light', 'Dark'],
+#         style={'width': '250px', 'margin': 'auto'}, 
+#         value=False
+#     ),
+
+#     daq.DarkThemeProvider([
+#   html.Link(
+#     href="https://codepen.io/anon/pen/BYEPbO.css",
+#     rel="stylesheet"
+#   ),
+#   html.Div([
+#     html.Div([
+#       html.H2('Controls'),
+#       dark_controls
+#     ], style={ 'width': '80%' })
+#   ],
+#   style={
+#     'width': '100%',
+#     'display': 'flex',
+#     'flexDirection': 'column',
+#     'alignItems': 'center',
+#     'justifyContent': 'center'
+#   })
+# ]),
+    
+#    builds the graph                    
+     html.Div([
 #    builds the graph                    
         dcc.Graph(
             id='HackerNews-graph',
@@ -98,7 +167,9 @@ app.layout = html.Div(children=[
     )],
     style={'width': '50%', 'display': 'inline-block'}),
     
-    html.Div([
+    
+    
+     html.Div([
         dcc.Graph(
             id='Reddit-graph',
             figure={
@@ -117,6 +188,7 @@ app.layout = html.Div(children=[
         }
     )],
     style={'width': '50%', 'display': 'inline-block'}),
+    
     html.Div(children ='Click data to Select',id='HackerNews-text',style={'width': '49%','display':'inline-block','vertical-align': 'top' }),
     html.Div(children ='Click data to Select',id='Reddit-text',style={'width': '49%','display':'inline-block','vertical-align': 'top'}),
     html.Div(children ='Hover over data to quick view',id='HackerNews-hover-text',style={'width': '49%','display':'inline-block','vertical-align': 'top' }),
@@ -125,6 +197,42 @@ app.layout = html.Div(children=[
 
 
 
+# @app.callback(
+#     [Output('dark-theme-feature','style'),
+#     Output('HackerNews-graph','style'),
+#     Output('Reddit-graph','style')],
+#     [Input('toggle-theme', 'value')]
+# )
+# def change_bg(dark_theme):
+#     if(dark_theme):
+#         return {'background-color': '#303030', 'color': 'white'}, {'plot_bgcolor': '#111111', 'paper_bgcolor': '#111111', 'font':'#7FDBFF'}, {'plot_bgcolor': '#111111', 'paper_bgcolor': '#111111', 'font':'#7FDBFF'}
+#     else:
+#         return {'background-color': 'white', 'color': 'black'}, {'plot_bgcolor': 'white', 'paper_bgcolor': 'black', 'font':'black'}, {'plot_bgcolor': 'white', 'paper_bgcolor': 'black', 'font':'black'}
+    
+                    
+# @app.callback(
+    
+# #    Output('Reddit-graph','style')],
+#     [Input('daq-light-dark-theme', 'value')]
+# )
+# def change_bg(dark_theme):
+#     if(dark_theme):
+#         return {'plot_bgcolor': '#111111', 'paper_bgcolor': '#111111', 'font':'#7FDBFF'}
+#     else:
+#         return {'plot_bgcolor': 'white', 'paper_bgcolor': 'black', 'font':'black'}
+
+    
+    
+# 'layout': {
+#                 'plot_bgcolor': colors['background'],
+#                 'paper_bgcolor': colors['background'],
+#                 'font': {
+#                     'color': colors['text']
+    
+#     colors = {
+#     'background': '#111111',
+#     'text': '#7FDBFF'
+                    
 @app.callback(
     dash.dependencies.Output('HackerNews-hover-text', 'children'),
     [dash.dependencies.Input('HackerNews-graph', 'hoverData')])
@@ -153,24 +261,48 @@ def update_text(hoverData):
      [dash.dependencies.Input('HackerNews-graph', 'clickData')])
 def update_text(clickData):
     if clickData['points'][0]['curveNumber']==0:
-        return HTMLParser.HTMLParser().unescape(hackernews_xamarin_Body_Data[clickData['points'][0]['pointIndex']])
+        return html.Div([
+            html.A("Direct link to HackerNews user comment", href="https://news.ycombinator.com/item?id="+str(hackernews_xamarin_Id_Data[clickData['points'][0]['pointIndex']]),target="_blank"),
+            html.H3(HTMLParser.HTMLParser().unescape(hackernews_xamarin_Body_Data[clickData['points'][0]['pointIndex']]))
+        ])
     if clickData['points'][0]['curveNumber']==1:
-        return HTMLParser.HTMLParser().unescape(hackernews_react_native_Body_Data[clickData['points'][0]['pointIndex']])
+        return html.Div([
+            html.A("Direct link to HackerNews user comment", href="https://news.ycombinator.com/item?id="+str(hackernews_react_native_Id_Data[clickData['points'][0]['pointIndex']]),target="_blank"),
+            html.H3(HTMLParser.HTMLParser().unescape(hackernews_react_native_Body_Data[clickData['points'][0]['pointIndex']]))
+        ])
     if clickData['points'][0]['curveNumber']==2:
-        return HTMLParser.HTMLParser().unescape(hackernews_flutter_Body_Data[clickData['points'][0]['pointIndex']])
+        return html.Div([
+            html.A("Direct link to HackerNews user comment", href="https://news.ycombinator.com/item?id="+str(hackernews_flutter_Id_Data[clickData['points'][0]['pointIndex']]),target="_blank"),
+            html.H3(HTMLParser.HTMLParser().unescape(hackernews_flutter_Body_Data[clickData['points'][0]['pointIndex']]))
+        ])
 
 @app.callback(
      dash.dependencies.Output('Reddit-text', 'children'),
      [dash.dependencies.Input('Reddit-graph', 'clickData')])
 def update_text(clickData):
     if clickData['points'][0]['curveNumber']==0:
-        return HTMLParser.HTMLParser().unescape(reddit_xamarin_Body_Data[clickData['points'][0]['pointIndex']])
+        return html.Div([
+            html.A("Direct link to Reddit user comment", href="https://new.reddit.com/comments/"+str(reddit_xamarin_link_Id_Data[clickData['points'][0]['pointIndex']]).replace("t3_","")+ "/_/" + str(reddit_xamarin_Id_Data[clickData['points'][0]['pointIndex']]),target="_blank"),
+            html.H3(HTMLParser.HTMLParser().unescape(reddit_xamarin_Body_Data[clickData['points'][0]['pointIndex']]))  
+        ])
     if clickData['points'][0]['curveNumber']==1:
-        return HTMLParser.HTMLParser().unescape(reddit_react_native_Body_Data[clickData['points'][0]['pointIndex']])
+        return html.Div([
+            html.A("Direct link to Reddit user comment", href="https://new.reddit.com/comments/"+str(reddit_react_native_link_Id_Data[clickData['points'][0]['pointIndex']]).replace("t3_","")+ "/_/" + str(reddit_react_native_Id_Data[clickData['points'][0]['pointIndex']]),target="_blank"),
+            html.H3(HTMLParser.HTMLParser().unescape(reddit_react_native_Body_Data[clickData['points'][0]['pointIndex']]))  
+        ])
     if clickData['points'][0]['curveNumber']==2:
-        return HTMLParser.HTMLParser().unescape(reddit_flutter_Body_Data[clickData['points'][0]['pointIndex']])    
+        return html.Div([
+            html.A("Direct link to Reddit user comment", href="https://new.reddit.com/comments/"+str(reddit_flutter_link_Id_Data[clickData['points'][0]['pointIndex']]).replace("t3_","")+ "/_/" + str(reddit_flutter_Id_Data[clickData['points'][0]['pointIndex']]),target="_blank"),
+            html.H3(HTMLParser.HTMLParser().unescape(reddit_flutter_Body_Data[clickData['points'][0]['pointIndex']]))  
+        ])   
     
     
+    
+#link_id     then    id
+#https://new.reddit.com/comments/hct9b/_/c1ufen4    
+    
+
+
 # @app.callback(
 #     dash.dependencies.Output('Reddit-hover-text', 'children'),
 #     [dash.dependencies.Input('Reddit-graph', 'hoverData')])
